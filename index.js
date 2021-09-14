97674e697814fecb0cb92db8079ab81d839d168a
Ruslan <ruslan@webservise.ru>
2021-09-14 10:18:38 +0300

    Правки 1.0

--- index.js
+++ index.js
@@ -4,70 +4,67 @@ const win0 = document.querySelector('.win_0');
 const resetButtons = document.querySelectorAll('.reset');
 const draw = document.querySelector('.draw');
 
-for (i = 1; i < 10; i++) {
+for (let i = 0; i < 9; i++) {
 	game.innerHTML += `<div class='block block${i}'></div>`;
 };
 
 const blocks = document.querySelectorAll('.block');
 
-let hod = 1;
+let currentPlayer = 1;
 
 for (let block of blocks) {
 	block.addEventListener ('click', function () {
 
-			if (block.textContent == 'X') {
-				hod = hod;
+		if (block.textContent == 'X') {
+			currentPlayer = currentPlayer;
+		}
+			
+		if (block.textContent == '0') {	
+			currentPlayer = currentPlayer;
+		}
+		
+		if (block.textContent == "") {
+			if (currentPlayer%2 == 0) {
+				block.textContent = 'X';
+				currentPlayer++;
+			}
+		
+			else {
+				block.textContent = '0';
+				currentPlayer++;
 			}
 			
-			if (block.textContent == '0') {
-				hod = hod;
-			}
-		
-			if (block.textContent == "") {
-				if (hod%2 == 0) {
-					block.textContent = 'X';
-					hod++;
-				}
-		
-				else {
-					block.textContent = '0';
-					hod++;
-				}
-		
-				
-				checkWinner();
-				resetBlocks();
-			}
+			checkWinnerAndDraw();
 		}
-	)
+		
+	})
 }
 
 
+//Функция срабатывает, когда кто-то (Х или 0) выиграет или же ничья
+function checkWinnerAndDraw () {
+	
+	if (blocks[0].textContent === 'X' && blocks[1].textContent === 'X' && blocks[2].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[3].textContent === 'X' && blocks[4].textContent === 'X' && blocks[5].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[6].textContent === 'X' && blocks[7].textContent === 'X' && blocks[8].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[0].textContent === 'X' && blocks[3].textContent === 'X' && blocks[6].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[1].textContent === 'X' && blocks[4].textContent === 'X' && blocks[7].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[2].textContent === 'X' && blocks[5].textContent === 'X' && blocks[8].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[0].textContent === 'X' && blocks[4].textContent === 'X' && blocks[8].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[2].textContent === 'X' && blocks[4].textContent === 'X' && blocks[6].textContent === 'X') {return winX.classList.add('active'); game.classList.add('wrapper_game')};
 
-function checkWinner () {
+	if (blocks[0].textContent === '0' && blocks[1].textContent === '0' && blocks[2].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[3].textContent === '0' && blocks[4].textContent === '0' && blocks[5].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[6].textContent === '0' && blocks[7].textContent === '0' && blocks[8].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[0].textContent === '0' && blocks[3].textContent === '0' && blocks[6].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[1].textContent === '0' && blocks[4].textContent === '0' && blocks[7].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[2].textContent === '0' && blocks[5].textContent === '0' && blocks[8].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[0].textContent === '0' && blocks[4].textContent === '0' && blocks[8].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+	if (blocks[2].textContent === '0' && blocks[4].textContent === '0' && blocks[6].textContent === '0') {return win0.classList.add('active'); game.classList.add('wrapper_game')};
+
 	if (blocks[0].textContent != "" && blocks[1].textContent != "" && blocks[2].textContent != "" && blocks[3].textContent != "" && blocks[4].textContent != "" && blocks[5].textContent != "" && blocks[6].textContent != "" && blocks[7].textContent != "" && blocks[8].textContent != "") {
 		draw.classList.add('active'); game.classList.add('wrapper_game');
-		resetBlocks();
 	}
-	
-	
-	if (blocks[0].textContent === 'X' && blocks[1].textContent === 'X' && blocks[2].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[3].textContent === 'X' && blocks[4].textContent === 'X' && blocks[5].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[6].textContent === 'X' && blocks[7].textContent === 'X' && blocks[8].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[0].textContent === 'X' && blocks[3].textContent === 'X' && blocks[6].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[1].textContent === 'X' && blocks[4].textContent === 'X' && blocks[7].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[2].textContent === 'X' && blocks[5].textContent === 'X' && blocks[8].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[0].textContent === 'X' && blocks[4].textContent === 'X' && blocks[8].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[2].textContent === 'X' && blocks[4].textContent === 'X' && blocks[6].textContent === 'X') {winX.classList.add('active'); game.classList.add('wrapper_game')};
-
-	if (blocks[0].textContent === '0' && blocks[1].textContent === '0' && blocks[2].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[3].textContent === '0' && blocks[4].textContent === '0' && blocks[5].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[6].textContent === '0' && blocks[7].textContent === '0' && blocks[8].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[0].textContent === '0' && blocks[3].textContent === '0' && blocks[6].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[1].textContent === '0' && blocks[4].textContent === '0' && blocks[7].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[2].textContent === '0' && blocks[5].textContent === '0' && blocks[8].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[0].textContent === '0' && blocks[4].textContent === '0' && blocks[8].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
-	if (blocks[2].textContent === '0' && blocks[4].textContent === '0' && blocks[6].textContent === '0') {win0.classList.add('active'); game.classList.add('wrapper_game')};
 
 	for (let resetButton of resetButtons)
 	resetButton.onclick = function () {
@@ -75,7 +72,7 @@ function checkWinner () {
 		win0.classList.remove('active');
 		draw.classList.remove('active');
 		game.classList.remove('wrapper_game');
-		hod = 1;
+		currentPlayer = 1;
 
 		for (let block of blocks) {
 			block.textContent = "";
